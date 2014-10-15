fbc780ea5d8e9cb8a9e92875edbadd33f0a4dee2
Rafal1 <rafal.zawadzki3@gmail.com>
2014-10-15 13:32:17 +0200

    second solid

--- app/canvas.js
+++ app/canvas.js
@@ -2,7 +2,7 @@
  * Created by Rafał Zawadzki on 2014-10-13.
  */
 var TRANSLATE_DEFAULT_STEP = 10;
-var CONSTANT_POSITION_D = 10;
+var CONSTANT_POSITION_D = 5;
 
 function Point3D(x, y, z) {
     this.x = x;
@@ -37,31 +37,20 @@ function Vector3D(A, B) {
     this.B = B;
 
     this.draw = draw;
+    this.projection = projection;
 
     function projection() {
-        //TODO 0.5 aby ładnie wyświetlało i niedosonałości obliczeń
-        //A.x = A.x * CONSTNT_POSITION_D / (A.z + CONSTNT_POSITION_D);
-        //A.y = A.y * CONSTNT_POSITION_D / (A.z + CONSTNT_POSITION_D);
-
-        //B.x = B.x * CONSTNT_POSITION_D / (B.z + CONSTNT_POSITION_D);
-        //B.y = B.y * CONSTNT_POSITION_D / (B.z + CONSTNT_POSITION_D);
-
-        this.A.x = A.x * CONSTANT_POSITION_D / (A.z);
-        this.A.y = A.y * CONSTANT_POSITION_D / (A.z);
-
-        this.B.x = B.x * CONSTANT_POSITION_D / (B.z);
-        this.B.y = B.y * CONSTANT_POSITION_D / (B.z);
+        var newCoordAfter = [];
+        newCoordAfter[0] = this.A.x * CONSTANT_POSITION_D / (this.A.z);
+        newCoordAfter[1] = this.A.y * CONSTANT_POSITION_D / (this.A.z);
+        newCoordAfter[2] = this.B.x * CONSTANT_POSITION_D / (this.B.z);
+        newCoordAfter[3] = this.B.y * CONSTANT_POSITION_D / (this.B.z);
+        return newCoordAfter;
     }
 
     function draw() {
-        // projection();
         var c = document.getElementById("myCanvas");
         var ctx = c.getContext("2d");
-
-//        this.A.x = this.A.x * CONSTANT_POSITION_D / (this.A.z);
-//        this.A.y = this.A.x * CONSTANT_POSITION_D / (this.A.z);
-//        this.B.x = this.A.x * CONSTANT_POSITION_D / (this.A.z);
-//        this.B.y = this.A.x * CONSTANT_POSITION_D / (this.A.z);
 
         //this.A = transformCoordinateSystem(this.A);
         //this.B = transformCoordinateSystem(this.B);
@@ -94,49 +83,17 @@ function transformCoordinateSystem(point) {
     return point;
 }
 
-//rectangles 80 / 100
-//var points = [];
-//points[0] = new Point3D(50, 150, 0);
-//points[1] = new Point3D(50 + 80, 150, 0);
-//points[2] = new Point3D(50 + 80, 150 + 100, 0);
-//points[3] = new Point3D(50, 150 + 100, 0);
-//
-//points[4] = new Point3D(50, 150, 5 + 30);
-//points[5] = new Point3D(50 + 80, 150, 5 + 30);
-//points[6] = new Point3D(50 + 80, 150 + 100, 5 + 30);
-//points[7] = new Point3D(50, 150 + 100, 5 + 30);
-//
-//var vectors = [];
-//vectors[0] = new Vector3D(points[0], points[1]);
-//vectors[1] = new Vector3D(points[1], points[2]);
-//vectors[2] = new Vector3D(points[2], points[3]);
-//vectors[3] = new Vector3D(points[3], points[0]);
-//
-//vectors[4] = new Vector3D(points[4], points[5]);
-//vectors[5] = new Vector3D(points[5], points[6]);
-//vectors[6] = new Vector3D(points[6], points[7]);
-//vectors[7] = new Vector3D(points[7], points[4]);
-
-//vectors[8] = new Vector3D(points[0], points[4]);
-//vectors[9] = new Vector3D(points[1], points[5]);
-//vectors[10] = new Vector3D(points[2], points[6]);
-//vectors[11] = new Vector3D(points[3], points[7]);
-
-//for (var i = 0; i < vectors.length; i++) {
-//    var tmpVe = vectors[i];
-//    tmpVe.draw();
-//}
-
 var points1 = [];
-points1[0] = new Point3D(-20, 20, 10);
-points1[1] = new Point3D(-40, 20, 10);
-points1[2] = new Point3D(-40, 40, 10);
-points1[3] = new Point3D(-20, 40, 10);
+//-40 + 30
+points1[0] = new Point3D(-20, -20, 1.3);
+points1[1] = new Point3D(-60, -20, 1.3);
+points1[2] = new Point3D(-60, 10, 1.3);
+points1[3] = new Point3D(-20, 10, 1.3);
 
-points1[4] = new Point3D(-20, 20, 30);
-points1[5] = new Point3D(-40, 20, 30);
-points1[6] = new Point3D(-40, 40, 30);
-points1[7] = new Point3D(-20, 40, 30);
+points1[4] = new Point3D(-20, -20, 2);
+points1[5] = new Point3D(-60, -20, 2);
+points1[6] = new Point3D(-60, 10, 2);
+points1[7] = new Point3D(-20, 10, 2);
 
 var vectors1 = [];
 vectors1[0] = new Vector3D(points1[0], points1[1]);
@@ -156,5 +113,38 @@ vectors1[11] = new Vector3D(points1[3], points1[7]);
 
 for (var i = 0; i < vectors1.length; i++) {
     var tmpVe = vectors1[i];
+    tmpVe.draw();
+}
+
+var points2 = [];
+//-30 + 70
+points2[0] = new Point3D(-20, -20, 2.2);
+points2[1] = new Point3D(-60, -20, 2.2);
+points2[2] = new Point3D(-60, 50, 2.2);
+points2[3] = new Point3D(-20, 50, 2.2);
+
+points2[4] = new Point3D(-20, -20, 2.8);
+points2[5] = new Point3D(-50, -20, 2.8);
+points2[6] = new Point3D(-50, 50, 2.8);
+points2[7] = new Point3D(-20, 50, 2.8);
+
+var vectors2 = [];
+vectors2[0] = new Vector3D(points2[0], points2[1]);
+vectors2[1] = new Vector3D(points2[1], points2[2]);
+vectors2[2] = new Vector3D(points2[2], points2[3]);
+vectors2[3] = new Vector3D(points2[3], points2[0]);
+
+vectors2[4] = new Vector3D(points2[4], points2[5]);
+vectors2[5] = new Vector3D(points2[5], points2[6]);
+vectors2[6] = new Vector3D(points2[6], points2[7]);
+vectors2[7] = new Vector3D(points2[7], points2[4]);
+
+vectors2[8] = new Vector3D(points2[0], points2[4]);
+vectors2[9] = new Vector3D(points2[1], points2[5]);
+vectors2[10] = new Vector3D(points2[2], points2[6]);
+vectors2[11] = new Vector3D(points2[3], points2[7]);
+
+for (var i = 0; i < vectors2.length; i++) {
+    var tmpVe = vectors2[i];
     tmpVe.draw();
 }
