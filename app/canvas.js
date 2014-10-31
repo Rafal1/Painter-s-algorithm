6fd8d26f7f51dfc9ffa22e912de77336a8da281f
Rafal1 <rafal.zawadzki3@gmail.com>
2014-10-31 14:22:47 +0100

    walls instead of vectors

--- app/canvas.js
+++ app/canvas.js
@@ -9,6 +9,8 @@ var ZOOM_CHANGE = 20;
 var ROTATE_X = 10;
 var ROTATE_Y = 10;
 var ROTATE_Z = 15;
+var CANVAS_WIDTH = 600;
+var CANVAS_HEIGHT = 400;
 
 function Wall3D(A, B, C, D) {
     this.VA = A;
@@ -20,76 +22,43 @@ function Wall3D(A, B, C, D) {
     this.draw = draw;
 
     function draw(ctx) {
-        var tmpVA, tmpVB, tmpVC;
-        tmpVA = projection(this.VA);
-        tmpVB = projection(this.VB);
-        tmpVC = projection(this.VC);
+        if(A.z<0 && B.z<0 && C.z<0 && D.z<0){
+            return;
+        }
+        var tmpPA, tmpPB, tmpPC, tmpPD;
+        tmpPA = projection(this.VA);
+        tmpPB = projection(this.VB);
+        tmpPC = projection(this.VC);
+        tmpPD = projection(this.VD);
 
-        var vectorEnding;
         ctx.beginPath();
         ctx.fillStyle = this.color;
-        ctx.moveTo(tmpVA.displayAX, tmpVA.displayAY);
-        ctx.lineTo(tmpVA.displayBX, tmpVA.displayBY);
-
-        if (tmpVA.displayBX == tmpVB.displayBX && tmpVA.displayBY == tmpVB.displayBY) {
-            vectorEnding = "A";
-            ctx.lineTo(tmpVB.displayAX, tmpVB.displayAY);
-        } else {
-            vectorEnding = "B";
-            ctx.lineTo(tmpVB.displayBX, tmpVB.displayBY);
-        }
-
-        if (vectorEnding == "A" && tmpVB.displayAX == tmpVC.displayBX && tmpVB.displayAY == tmpVC.displayBY) {
-            ctx.lineTo(tmpVC.displayAX, tmpVC.displayAY);
-        } else if (vectorEnding == "B" && tmpVB.displayBX == tmpVC.displayBX && tmpVB.displayBY == tmpVC.displayBY) {
-            ctx.lineTo(tmpVC.displayAX, tmpVC.displayAY);
-        } else {
-            ctx.lineTo(tmpVC.displayBX, tmpVC.displayBY);
-        }
-
-        ctx.lineTo(tmpVA.displayAX, tmpVA.displayAY);
+//        ctx.strokeStyle = "#FFFF00";
+        ctx.moveTo(tmpPA.displayX, tmpPA.displayY);
+        ctx.lineTo(tmpPB.displayX, tmpPB.displayY);
+        ctx.lineTo(tmpPC.displayX, tmpPC.displayY);
+        ctx.lineTo(tmpPD.displayX, tmpPD.displayY);
         ctx.closePath();
         ctx.fill();
-
-        var d = new Date();
+//        ctx.stroke();
     }
+
 }
 
 function projection(v) {
-    if (v.A.z <= 0 && v.B.z <= 0) { //not true
-        return {
-            displayAX: undefined,
-            displayAY: undefined,
-            displayBX: undefined,
-            displayBY: undefined
-        }
-    }
-    var tmpAX, tmpAY, tmpBX, tmpBY, tmpA, tmpB;
-    tmpAX = v.A.x * ZOOM_COEFFICIENT / (v.A.z);
-    tmpAY = v.A.y * ZOOM_COEFFICIENT / (v.A.z);
-    tmpBX = v.B.x * ZOOM_COEFFICIENT / (v.B.z);
-    tmpBY = v.B.y * ZOOM_COEFFICIENT / (v.B.z);
+    var tmpAX, tmpAY;
+    tmpAX = v.x * ZOOM_COEFFICIENT / (v.z);
+    tmpAY = v.y * ZOOM_COEFFICIENT / (v.z);
 
-    if (v.A.z <= 0) {
-        tmpA = notVisible(v.B, v.A);
-        tmpAX = tmpA.x * ZOOM_COEFFICIENT / (tmpA.z);
-        tmpAY = tmpA.y * ZOOM_COEFFICIENT / (tmpA.z);
+    if (v.z <= 0) {
+        var tmpZ = 0.01;
+        tmpAX = v.x * ZOOM_COEFFICIENT / (tmpZ);
+        tmpAY = v.y * ZOOM_COEFFICIENT / (tmpZ);
     }
-
-    if (v.B.z <= 0) {
-        tmpB = notVisible(v.A, v.B);
-        tmpBX = tmpB.x * ZOOM_COEFFICIENT / (tmpB.z);
-        tmpBY = tmpB.y * ZOOM_COEFFICIENT / (tmpB.z);
-    }
-
     var PointATransformSystem = transformCoordinateSystem(tmpAX, tmpAY);
-    var PointBTransformSystem = transformCoordinateSystem(tmpBX, tmpBY);
-
     return {
-        displayAX: PointATransformSystem.transformedX,
-        displayAY: PointATransformSystem.transformedY,
-        displayBX: PointBTransformSystem.transformedX,
-        displayBY: PointBTransformSystem.transformedY
+        displayX: PointATransformSystem.transformedX,
+        displayY: PointATransformSystem.transformedY
     }
 }
 
@@ -161,49 +130,6 @@ function Point3D(x, y, z) {
     }
 }
 
-function Vector3D(A, B) {
-    this.A = A;
-    this.B = B;
-    this.color = "#000000";
-
-    this.draw = draw;
-
-    function draw(ctx) {
-        if (this.A.z <= 0 && this.B.z <= 0) {
-            return;
-        }
-
-        var tmpAX, tmpAY, tmpBX, tmpBY, tmpA, tmpB;
-
-        tmpAX = this.A.x * ZOOM_COEFFICIENT / (this.A.z);
-        tmpAY = this.A.y * ZOOM_COEFFICIENT / (this.A.z);
-        tmpBX = this.B.x * ZOOM_COEFFICIENT / (this.B.z);
-        tmpBY = this.B.y * ZOOM_COEFFICIENT / (this.B.z);
-
-        if (this.A.z <= 0) {
-            tmpA = notVisible(this.B, this.A);
-            tmpAX = tmpA.x * ZOOM_COEFFICIENT / (tmpA.z);
-            tmpAY = tmpA.y * ZOOM_COEFFICIENT / (tmpA.z);
-        }
-
-        if (this.B.z <= 0) {
-            tmpB = notVisible(this.A, this.B);
-            tmpBX = tmpB.x * ZOOM_COEFFICIENT / (tmpB.z);
-            tmpBY = tmpB.y * ZOOM_COEFFICIENT / (tmpB.z);
-        }
-
-        var PointATransformSystem = transformCoordinateSystem(tmpAX, tmpAY);
-        var PointBTransformSystem = transformCoordinateSystem(tmpBX, tmpBY);
-
-        ctx.beginPath();
-        ctx.strokeStyle = this.color;
-        ctx.moveTo(PointATransformSystem.transformedX, PointATransformSystem.transformedY);
-        ctx.lineTo(PointBTransformSystem.transformedX, PointBTransformSystem.transformedY);
-        ctx.closePath();
-        ctx.stroke();
-    }
-}
-
 function notVisible(vis, notvis) {
     var p = new Point3D();
     p.z = 0.01;
@@ -217,8 +143,8 @@ function notVisible(vis, notvis) {
 }
 
 function transformCoordinateSystem(Bx, By) {
-    Bx = Bx + 300;
-    By = -By + 200;
+    Bx = Bx + CANVAS_WIDTH / 2;
+    By = -By + CANVAS_HEIGHT / 2;
     return {
         transformedX: Bx,
         transformedY: By
@@ -226,52 +152,21 @@ function transformCoordinateSystem(Bx, By) {
 }
 
 function makeSolidVectorsFromPoints(points, color) {
-    var vectors = [];
-    vectors[0] = new Vector3D(points[0], points[1]);
-    vectors[0].color = color;
-    vectors[1] = new Vector3D(points[1], points[2]);
-    vectors[1].color = color;
-    vectors[2] = new Vector3D(points[2], points[3]);
-    vectors[2].color = color;
-    vectors[3] = new Vector3D(points[3], points[0]);
-    vectors[3].color = color;
-
-    vectors[4] = new Vector3D(points[4], points[5]);
-    vectors[4].color = color;
-    vectors[5] = new Vector3D(points[5], points[6]);
-    vectors[5].color = color;
-    vectors[6] = new Vector3D(points[6], points[7]);
-    vectors[6].color = color;
-    vectors[7] = new Vector3D(points[7], points[4]);
-    vectors[7].color = color;
-
-    vectors[8] = new Vector3D(points[0], points[4]);
-    vectors[8].color = color;
-    vectors[9] = new Vector3D(points[1], points[5]);
-    vectors[9].color = color;
-    vectors[10] = new Vector3D(points[2], points[6]);
-    vectors[10].color = color;
-    vectors[11] = new Vector3D(points[3], points[7]);
-    vectors[11].color = color;
-
     var walls = [];
-    //vector sequence is imortatnt (unfortunately ;()
-    walls[0] = new Wall3D(vectors[0], vectors[1], vectors[2], vectors[3]);
+    walls[0] = new Wall3D(points[0], points[1], points[2], points[3]);
     walls[0].color = color;
-    walls[1] = new Wall3D(vectors[4], vectors[5], vectors[6], vectors[7]);
+    walls[1] = new Wall3D(points[4], points[5], points[6], points[7]);
     walls[1].color = color;
-    walls[2] = new Wall3D(vectors[8], vectors[7], vectors[11], vectors[3]);
+    walls[2] = new Wall3D(points[0], points[3], points[7], points[4]);
     walls[2].color = color;
-    walls[3] = new Wall3D(vectors[0], vectors[9], vectors[4], vectors[8]);
+    walls[3] = new Wall3D(points[0], points[1], points[5], points[4]);
     walls[3].color = color;
-    walls[4] = new Wall3D(vectors[1], vectors[10], vectors[5], vectors[9]);
+    walls[4] = new Wall3D(points[1], points[5], points[6], points[2]);
     walls[4].color = color;
-    walls[5] = new Wall3D(vectors[2], vectors[11], vectors[6], vectors[10]);
+    walls[5] = new Wall3D(points[3], points[2], points[6], points[7]);
     walls[5].color = color;
-
     return {
-        walls: walls,
-        vectors: vectors
+        walls: walls
     }
 }
 
@@ -349,15 +244,15 @@ function rotatePicture(points, direction) {
     }
 }
 
-function drawScene(vectors, walls) {
+function drawScene(walls) {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.clearRect(0, 0, c.width, c.height);
     ctx.beginPath();
-    for (var i = 0; i < vectors.length; i++) {
-        var tmpVe = vectors[i];
-        tmpVe.draw(ctx);
-    }
+//    for (var i = 0; i < vectors.length; i++) {
+//        var tmpVe = vectors[i];
+//        tmpVe.draw(ctx);
+//    }
     for (var i = 0; i < walls.length; i++) {
         walls[i].draw(ctx);
     }
@@ -367,63 +262,92 @@ function controlSystem(event) {
     switch (event.keyCode) {
         case 87: //w
             tanslatePicture(allPoints, "up");
-            drawScene(allVectors, allWalls); // ;/ when defined? in global pool only :(
+            drawScene(allWalls); // ;/ when defined? in global pool only :(
             break;
         case 83: //s
             tanslatePicture(allPoints, "down");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 65: //a
             tanslatePicture(allPoints, "left");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 68: //d
             tanslatePicture(allPoints, "right");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 81: //q
             tanslatePicture(allPoints, "forward");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 69: //e
             tanslatePicture(allPoints, "back");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 80: //p
             ZOOM_COEFFICIENT = ZOOM_COEFFICIENT + ZOOM_CHANGE
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 79: //o
             ZOOM_COEFFICIENT = ZOOM_COEFFICIENT - ZOOM_CHANGE
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 85: //u
             rotatePicture(allPoints, "XB");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 89: //y
             rotatePicture(allPoints, "XF");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 78: //n
             rotatePicture(allPoints, "ZF");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 77: //m
             rotatePicture(allPoints, "ZB");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 72: //h
             rotatePicture(allPoints, "YF");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         case 74: //j
             rotatePicture(allPoints, "YB");
-            drawScene(allVectors, allWalls);
+            drawScene(allWalls);
             break;
         default:
             break;
     }
+}
+
+function Wall3DParam(ID, SC, X, Y) {
+    this.ID = ID;
+    this.SC = SC;
+    this.XSC = X;
+    this.YSC = Y;
+}
+
+function scOfWall(wall) {
+//    wall.A
+}
+
+function remakeWalls(walls) {
+    var remaked = [];
+    for (var i = 0; walls.length; i++) {
+        var sc = walls[i];
+        remaked[i] = new Wall3DParam();
+        walls[i]
+    }
+
+    return remaked;
+}
+
+function sortWalls(walls) {
+    var sortedWalls = [];
+    sortedWalls = walls;
+    return sortedWalls;
+
 }
 
 var points1 = [];
@@ -465,8 +389,9 @@ points3[7] = new Point3D(20, 5, 70);
 var solid1 = makeSolidVectorsFromPoints(points1, "#FF0000"); //red
 var solid2 = makeSolidVectorsFromPoints(points2, "#000000"); //black
 var solid3 = makeSolidVectorsFromPoints(points3, "#00CC00"); //green
-var allVectors = solid1.vectors.concat(solid2.vectors).concat(solid3.vectors);
+//var allVectors = solid1.vectors.concat(solid2.vectors).concat(solid3.vectors);
 var allPoints = points1.concat(points2).concat(points3);
 var allWalls = solid1.walls.concat(solid2.walls).concat(solid3.walls);
+allWalls = sortWalls(allWalls);
 
-drawScene(allVectors, allWalls);
+drawScene(allWalls);
